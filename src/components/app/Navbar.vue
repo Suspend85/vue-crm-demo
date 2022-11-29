d04d019a34462a508480d2ac817553196992b983
suspend85 <melomanko@gmail.com>
2022-11-29 15:23:06 +0700

    navbar, time functional added

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,29 +5,30 @@
         <a href="#" @click.prevent="$emit('toggle')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ dateFilter() }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
         <li>
-          <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+          <a class="dropdown-trigger black-text" href="#" data-target="dropdown" ref="dropdown">
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
           </ul>
+
         </li>
       </ul>
     </div>
@@ -35,8 +36,53 @@
 </template>
 
 <script>
-export default {
 
+export default {
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null
+  }),
+
+
+  methods: {
+    logout() {
+      console.log('logout');
+      this.$router.push('/login?message=logout')
+    },
+
+    dateFilter(value, format = 'datetime') {
+      value = this.date
+      const options= {}
+      if (format.includes('time')) {
+        options.day = '2-digit'
+        options.month = 'long'
+        options.year = 'numeric'
+        options.hour = '2-digit'
+        options.minute = '2-digit'
+        options.second = '2-digit'
+      }
+      return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
+    }
+  },
+
+  mounted() {
+    this.interval = setInterval(() =>{
+      this.date = new Date()
+    }, 1000)
+
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    })
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount');
+    clearInterval(this.interval)
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy()
+    }
+  },
+  // components: ['dateFilter']
 };
 </script>
 
